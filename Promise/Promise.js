02fed0c05580fb42a6154ebbd9950b58950f070a
wangzilong1997 <1321787581@qq.com>
2021-05-17 07:43:14 +0800

    Promise自定义封装-异步任务 then 方法实现

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -4,12 +4,20 @@ function Promise(executor){
     
     let self = this
 
+    //声明回调函数
+    this.callback = {}
+
     function resolve(data){
         //判断状态 
         if (self.PromiseState !== 'pending') return
 
         self.PromiseState = 'fulfilled'
         self.PromiseResult = data
+
+        //调用成功的回调函数
+        if(self.callback.onResolved){
+            self.callback.onResolved(data)
+        }
 
     }
 
@@ -19,6 +27,12 @@ function Promise(executor){
 
         self.PromiseState = 'reject'
         self.PromiseResult = data
+
+       
+        //调用失败时候的回调
+        if(self.callback.onRejected){
+            self.callback.onRejected(data)
+        }
     }
 
     try{
@@ -38,6 +52,17 @@ function Promise(executor){
         if(this.PromiseState === 'reject'){
             onRejected(this.PromiseResult)
         }
+
+        //判断pending状态
+    if(this.PromiseState === 'pending'){
+        //保存回调函数
+
+        this.callback = {
+            onResolved:onResolved,
+            onRejected:onRejected
+        }
+
+    }
     
     }
 }
