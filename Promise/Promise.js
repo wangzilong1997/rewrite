a32537d22932accc52b36b98d28fb1ea732204a5
wangzilong1997 <1321787581@qq.com>
2021-05-17 07:04:39 +0800

    声明实现函数resolve 和reject 保存this指向

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -1,7 +1,19 @@
 function Promise(executor){
+    this.PromiseState = 'pending'
+    this.PromiseResult = null
+    
+    let self = this
 
-  
+    function resolve(data){
+        self.PromiseState = 'fulfilled'
+        self.PromiseResult = data
 
+    }
+
+    function reject(data){
+        self.PromiseState = 'reject'
+        self.PromiseResult = data
+    }
     //同步调用【执行器函数】
     executor(resolve,reject)
 }
